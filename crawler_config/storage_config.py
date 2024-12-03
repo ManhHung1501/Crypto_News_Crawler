1852ad7765c3b8cb8e563c6c068269fe34d727d0
ManhHung1501 <hungnm2@ftech.ai>
2024-12-03 15:15:43 +0700

    update

--- crawler_config/storage_config.py
+++ crawler_config/storage_config.py
@@ -5,9 +5,7 @@ from crawler_utils.common_utils import project_dir
 # Load the .env file
 load_dotenv(f"{project_dir}/.env")
 
-MINIO_HOST = os.getenv("MINIO_HOST")
-MINIO_PORT = os.getenv("MINIO_PORT")
-MINIO_ENDPOINT = f"{MINIO_HOST}:{MINIO_PORT}"
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
 ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
 CRYPTO_NEWS_BUCKET = os.getenv("MINIO_BUCKET")
