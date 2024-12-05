16f3a6234c1c6e18330f9dbe26d564863c28b6d4
ManhHung1501 <hungnm2@ftech.ai>
2024-12-05 11:18:26 +0700

    update

--- dags/full_crawl_dag.py
+++ dags/full_crawl_dag.py
@@ -13,7 +13,7 @@ default_args = {
     'email_on_failure': False,
     "start_date": datetime(2024, 10, 1),
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
